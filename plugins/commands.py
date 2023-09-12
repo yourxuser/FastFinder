a64d8a64b8f2d1a8349719902957232e849ef4b3
Eight22 <144815032+Eight22@users.noreply.github.com>
2023-09-12 21:26:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -47,7 +47,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/PostQutoBot?startgroup=true')
             ],[
             InlineKeyboardButton('🙋🏻‍♀️ Help', callback_data='help'),
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
@@ -96,7 +96,7 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/PostQutoBot?startgroup=true')
             ],[
             InlineKeyboardButton('🙋🏻‍♀️ Help', callback_data='help'),
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
