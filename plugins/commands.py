56487913852c7f5a08627e69cd90da9fc387f478
yourxuser <145739154+yourxuser@users.noreply.github.com>
2023-09-22 10:28:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
             [
-                InlineKeyboardButton('🙋 Help', url=f"https://t.me/PostQutoBot?start=help"),
+                InlineKeyboardButton('🙋 Help', url=f"https://t.me/FastFinderBot?start=help"),
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -39,7 +39,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/PostQutoBot?startgroup=true')
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/FastFinderBot?startgroup=true')
             ],[
             InlineKeyboardButton('🙋🏻‍♀️ Help', callback_data='help'),
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
@@ -73,7 +73,7 @@ async def start(client, message):
                 pre = 'checksubp' if kk == 'filep' else 'checksub' 
                 btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
             except (IndexError, ValueError):
-                btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
+                btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/+pQ4_XH4_1FBmMjIx?start={message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
             text="**Please Join My Updates Channel to use this Bot!**",
@@ -83,7 +83,7 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/PostQutoBot?startgroup=true')
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/FastFinderBot?startgroup=true')
             ],[
             InlineKeyboardButton('🙋🏻‍♀️ Help', callback_data='help'),
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
